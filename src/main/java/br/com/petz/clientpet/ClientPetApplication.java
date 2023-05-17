b680f7324851098edff463fba95dcb66b864b5d6
Gsfantasma <gsglauco@gmail.com>
2023-05-17 15:04:37 -0300

    inicia o projeto

---
+++ src/main/java/br/com/petz/clientpet/ClientPetApplication.java
@@ -0,0 +1,13 @@
+package br.com.petz.clientpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientPetApplication.class, args);
+	}
+
+}
