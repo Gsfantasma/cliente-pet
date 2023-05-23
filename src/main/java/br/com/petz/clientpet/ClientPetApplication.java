3a66ca90bb80f356fd9563ded9f3bbe232ee166d
Gsfantasma <gsglauco@gmail.com>
2023-05-23 19:04:21 -0300

    teste clientePetAplication

--- src/main/java/br/com/petz/clientpet/ClientPetApplication.java
+++ src/main/java/br/com/petz/clientpet/ClientPetApplication.java
@@ -2,9 +2,17 @@ package br.com.petz.clientpet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientPetApplication {
+	
+	public String getHomeTeste() {
+		return "Cliente Pet - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientPetApplication.class, args);
